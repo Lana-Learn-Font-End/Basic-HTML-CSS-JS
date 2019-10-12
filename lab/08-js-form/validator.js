026f92451c07905a42acff940c5995743e970eac
Lana <sitdownrightnow2552@gmail.com>
2019-10-12 12:52:41 +0700

    simple validator class

--- lab/08-js-form/validator.js
+++ lab/08-js-form/validator.js
@@ -1,22 +1,100 @@
 class InputValidator {
-    constructor(inputElement) {
-        this.input = inputElement;
-        this.errors = {}
+    constructor(id) {
+        this.input = document.getElementById(id);
+        this.message = document.getElementById(`${id}-error`);
+        this.input.addEventListener("keyup", () => this.validate());
+
+        this.errorMessage = "Invalid";
+        this.validMessage = "";
+        this.validateFns = [];
     }
 
-    isBlank(message) {
-        return !input.value.trim();
+    validate() {
+        this.errorMessage = "";
+        this.validateFns.forEach(fn => fn());
+        if (this.hasError()) {
+            this.input.dataset.validate = "invalid";
+            this.message.dataset.validate = "invalid";
+            this.message.innerText = this.errorMessage;
+        } else {
+            this.input.dataset.validate = "valid";
+            this.message.dataset.validate = "valid";
+            this.message.innerText = this.validMessage;
+        }
+    }
+
+    hasError() {
+        // cast string to boolean, then put one more !
+        // to reverse the boolean value
+        return !!this.errorMessage;
+    }
+
+    setErrorMessage(message) {
+        this.errorMessage = message;
+    }
+
+    setValidMessage(message) {
+        this.validMessage = message;
+    }
+
+    required(message) {
+        this.validateFns.push(() => {
+            if (!this.input.value) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
+    }
+}
+
+class TextInputValidator extends InputValidator {
+    constructor(id) {
+        super(id);
+    }
+
+    notBlank(message) {
+        this.validateFns.push(() => {
+            if (!this.input.value.trim()) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
+    }
+
+    matchPattern(pattern, message) {
+        this.validateFns.push(() => {
+            if (!this.input.value.match(pattern)) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
+    }
+
+    maxLength(max, message) {
+        this.validateFns.push(() => {
+            if (this.input.value > max) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
+    }
+
+    minLength(min, message) {
+        this.validateFns.push(() => {
+            if (this.input.value < min) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
     }
 
     isEmail(message) {
-        return input.value.match(/^\w+@\w+(\.\w+)+\s*$/);
-    }
-
-    isNumber(message) {
-        const value = input.value;
-        if (value === 0 || value) {
-            return !isNaN(Number(value));
-        }
-        return false;
+        this.validateFns.push(() => {
+            const emailRegex = /^\w+@\w+(\.\w+)+\s*$/;
+            if (!this.input.value.match(emailRegex)) {
+                this.setErrorMessage(message);
+            }
+        });
+        return this;
     }
 }
