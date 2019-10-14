43968dcc2c945229756b48038e63f7c8eb33ca7f
lana <sitdownrightnow2552@gmail.com>
2019-10-14 11:43:57 +0700

    add more validator

--- demo/validator-lib/validator.js
+++ demo/validator-lib/validator.js
@@ -1,13 +1,30 @@
 function isEmail(str) {
-    if (str) {
-        return str.match(/^\w+@\w+(\.\w+)+\s*$/);
+    return str.match(/^\w+@\w+(\.\w+)+\s*$/);
+}
+
+function notBlank(str) {
+    return !!str.trim();
+}
+
+function matchPattern(str, pattern) {
+    return str.match(pattern);
+}
+
+function isNumber(str) {
+    if (str === 0 || str) {
+        return !isNaN(Number(str));
     }
     return false;
 }
 
-function notBlank(str) {
-    if (str) {
-        return str.trim();
-    }
-    return false;
+function maxLength(str, max) {
+    return str.length <= max;
+}
+
+function minLength(str, min) {
+    return str.length >= min;
+}
+
+function notEmpty(str) {
+    return !!str;
 }
